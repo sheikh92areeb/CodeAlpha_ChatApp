574f12948d7e9511385c36a2d6ccc26ab7e4d505
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-07-22 14:51:07 +0500

    Update AuthContext.jsx

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import axios from 'axios';
 import toast from "react-hot-toast";
+import { io } from 'socket.io-client'
 
 const backendurl = import.meta.env.VITE_BACKEND_URL;
 axios.defaults.baseURL = backendurl;
@@ -19,11 +20,57 @@ export const AuthProvider = ({ children }) => {
         try {
             const { data } = await axios.get('/api/auth/check');
             if (data.susscee) {
-                setAuthUser(data.user)
+                setAuthUser(data.user);
+                connectSocket(data.user);
             }
         } catch (error) {
-            toast.error(error.message)
+            toast.error(error.message);
         }
+    }
+
+    // Login function to handle user authentication and socket connection
+    const login = async (state, credentials) => {
+        try {
+            const { data } = axios.post(`/api/auth/${state}`, credentials);
+            if (data.success) {
+                setAuthUser(data.userData);
+                connectSocket(data.userData);
+                axios.defaults.headers.common["token"] = data.token;
+                setToken(data.token);
+                localStorage.setItem("token", data.token);
+                toast.success(data.message);
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
+    }
+
+    // Logout function to handle user logout and socket disconnection
+    const logout = async () => {
+        localStorage.removeItem("token");
+        setToken(null);
+        setAuthUser(null);
+        setOnlineUser([]);
+        axios.defaults.headers.common["token"] = null;
+        toast.success("Logged out Successfully");
+        socket.disconnect();
+    }
+
+    // Connect socket function to handle socket connection and online users update
+    const connectSocket = (userData) => {
+        if (!userData || socket?.connected) return
+        const newSocket = io(backendurl, {
+            query: {
+                userId: userData._id
+            }
+        });
+        newSocket.connect();
+        setSocket(newSocket);
+        newSocket.on("getOnlineUsers", (userIds)=> {
+            setOnlineUser(userIds);
+        });
     }
 
     useEffect(()=>{
